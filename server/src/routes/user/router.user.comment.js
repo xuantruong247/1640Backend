3568614873b8a43bf5a535cefc09aa1d189a2ece
Nguyễn Trường <cutruong24701@gmail.com>
2023-04-12 00:18:29 +0700

    sent mail

--- server/src/routes/user/router.user.comment.js
+++ server/src/routes/user/router.user.comment.js
@@ -4,6 +4,10 @@ const { verifyToken } = require("../../middlewares/auth")
 const Idea = require('../../models/idea')
 const moment = require("moment-timezone")
 const vnTimezone = "Asia/Ho_Chi_Minh";
+const nodemailer = require("nodemailer")
+const User = require("../../models/user")
+
+
 
 const { BadRequestError } = require('../../utils/error-app')
 
@@ -12,13 +16,43 @@ router.post('/comments/:id', verifyToken, async (req, res, next) => {
     try {
         const idea = await Idea.findById(req.params.id)
         if (!idea) throw new BadRequestError("Idea not found")
+
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "truongnxgcs190087@fpt.edu.vn",
+                pass: "dqmnztbuigyetsov"
+            }
+        })
+
+        const user = await User.findOne({ _id: req.user._id });
+        const userEmail = user.profile.email;
+
         const comment = {
             content: req.body.content,
-            userId: req.user,
+            userId: user._id,
             created_at: moment.tz(vnTimezone).format("YYYY-MM-DDTHH:mm:ss.SSS")
-            // created_at: moment.tz(vnTimezone).format("YYYY-MM-DDTHH:mm:ss.SSS[Z]")
+
         }
         console.log("comment", comment);
+
+        // send email
+        const mailOption = {
+            from: "truongnxgcs190087@fpt.edu.vn",
+            to: userEmail,
+
+            subject: "Thông báo: Bạn vừa comment 1 nội dung",
+            text: "Bạn vừa comment một nội dung ở web demo của chúng tôi."
+        }
+        transporter.sendMail(mailOption, function (err, info) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Email sent comment: " + info.response);
+            }
+        })
+
         idea.comments.push(comment)
         await idea.save()
         res.json(comment)
--- server/src/routes/user/router.user.reaction.js
+++ server/src/routes/user/router.user.reaction.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const Idea = require('../../models/idea');
 const { verifyToken } = require("../../middlewares/auth");
 const { BadRequestError } = require('../../utils/error-app');
+const nodemailer = require("nodemailer")
+const User = require("../../models/user")
 
 router.post('/like/:id', verifyToken, async (req, res, next) => {
   try {
@@ -10,8 +12,19 @@ router.post('/like/:id', verifyToken, async (req, res, next) => {
     const idea = await Idea.findById(ideaId);
     if (!idea) throw new BadRequestError("idea not found");
 
-    const user = req.user;
-    console.log(user);
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "truongnxgcs190087@fpt.edu.vn",
+        pass: "dqmnztbuigyetsov"
+      }
+    })
+
+    const user = await User.findOne({ _id: req.user._id });
+    const userEmail = user.profile.email;
+
+
+    console.log(userEmail);
     const like = idea.likes.find((like) => like.likesId && like.likesId.toString() === user._id.toString());
 
     const dislike = idea.dislikes.find((dislike) => dislike.dislikesId && dislike.dislikesId.toString() === user._id.toString())
@@ -26,8 +39,22 @@ router.post('/like/:id', verifyToken, async (req, res, next) => {
       await idea.save();
       return res.status(200).send({ message: "Đã dislike và chuyển sang like" });
     }
-
     idea.likes.push({ likesId: user._id });
+    // send email
+    const mailOption = {
+      from: "truongnxgcs190087@fpt.edu.vn",
+      to: userEmail,
+
+      subject: "Thông báo: Bạn vừa like 1 nội dung",
+      text: "Bạn vừa like một nội dung ở web demo của chúng tôi."
+    }
+    transporter.sendMail(mailOption, function (err, info) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Email sent like: " + info.response);
+      }
+    })
     await idea.save();
     res.status(200).send({ message: "Đã like" });
 
@@ -38,15 +65,21 @@ router.post('/like/:id', verifyToken, async (req, res, next) => {
 
 
 
-
 router.post('/dislike/:id', verifyToken, async (req, res, next) => {
   try {
     const ideaId = req.params.id;
     const idea = await Idea.findById(ideaId);
     if (!idea) throw new BadRequestError("idea not found");
 
-    const user = req.user;
-    console.log(user);
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "truongnxgcs190087@fpt.edu.vn",
+        pass: "dqmnztbuigyetsov"
+      }
+    })
+    const user = await User.findOne({ _id: req.user._id });
+    const userEmail = user.profile.email;
     const like = idea.likes.find((like) => like.likesId && like.likesId.toString() === user._id.toString());
 
     const dislike = idea.dislikes.find((dislike) => dislike.dislikesId && dislike.dislikesId.toString() === user._id.toString())
@@ -62,6 +95,21 @@ router.post('/dislike/:id', verifyToken, async (req, res, next) => {
       return res.status(200).send({ message: "Đã like và chuyển sang dislike" });
     }
     idea.dislikes.push({ dislikesId: user._id });
+    // send email
+    const mailOption = {
+      from: "truongnxgcs190087@fpt.edu.vn",
+      to: userEmail,
+
+      subject: "Thông báo: Bạn vừa dislike 1 nội dung",
+      text: "Bạn vừa dislike một nội dung ở web demo của chúng tôi."
+    }
+    transporter.sendMail(mailOption, function (err, info) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Email sent dislike: " + info.response);
+      }
+    })
     await idea.save();
     res.status(200).send({ message: "Đã like" });
 
